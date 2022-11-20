dbefc8ee9c2247a3e83027e4b1e50d1f819d0828
CrowChi <saksitsanchai.next@gmail.com>
2022-11-20 18:17:34 +0700

    Lab

---
+++ src/LAB/LAB01.java
@@ -0,0 +1,9 @@
+package LAB;
+
+public class LAB01 {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+	}//END PRINT
+}//EMD main
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author saksi
+ *
+ */
+module LAB {
+}
